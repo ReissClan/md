954fa4a45f0fdcd2f676ca8514d4fec5799a164c
darkdash4real <78483695+darkdash4real@users.noreply.github.com>
2021-02-06 21:50:25 +0300

    Add files via upload

--- md.py
+++ md.py
@@ -24,7 +24,9 @@ time.sleep(0.5)
 print("\n")
 client=amino.Client() 
 client.login(email=email,password=password)
-thelink=input("\033[0;37m"+"CHAT LINK: ")
+thelink=input("\033[0;32m"+"CHAT LINK: ")
+time.sleep(0.5)
+print("\n")
 gpid=client.get_from_code(thelink)
 chatid=gpid.objectId
 comid=gpid.path[1:gpid.path.index("/")]
@@ -35,5 +37,5 @@ while True:
     for msgId,msg in zip(msglist.messageId,msglist.content):
     	subclient.delete_message(chatId=chatid,messageId=msgId)
     	print("[DONE] (TO EXIT THE TOOL CLOSE THE PAGE OR: CTRL+C")
-    	
-		
+else:
+	pass
